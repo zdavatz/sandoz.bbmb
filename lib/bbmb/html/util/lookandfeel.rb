d78bdb7375d9201ac2c36ebfe4a2f47f16d3b2aa
Hannes Wyss <hwyss@ywesee.com>
2009-01-16 14:38:17 +0100

    Replace the Contact-Information on Startpage

--- lib/bbmb/html/util/lookandfeel.rb
+++ lib/bbmb/html/util/lookandfeel.rb
@@ -156,7 +156,7 @@ Ein Administrator wurde automatisch darüber informiert und wird mit Ihnen Konta
       :unavailable0             =>  "Unidentifiziertes Produkt (",
       :unavailable1             =>  ")",
       :version                  =>  "Commit-ID",
-      :welcome                  =>  "Willkommen bei Sandoz&nbsp;Pharmaceuticals&nbsp;AG\nBei Fragen wenden Sie sich bitte an den Customer&nbsp;Service (Tobias Wyss und Alessandro Pezzuca) Tel.&nbsp;0800&nbsp;858&nbsp;885",
+      :welcome                  =>  "Willkommen bei Sandoz&nbsp;Pharmaceuticals&nbsp;AG\nBei Fragen wenden Sie sich bitte an den Customer&nbsp;Service (<a href='mailto:Customer.Service_ch@sandoz.com>Customer.Service_ch@sandoz.com</a>) Tel.&nbsp;0800&nbsp;858&nbsp;885",
       :ywesee                   =>  "ywesee.com",
     }
   }
